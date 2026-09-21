a2425107d95d1aa99dd488b9408e856a53ebf906
rkalema <rkalema26@gmail.com>
2026-09-21 15:19:40 -0700

    Add Evidence Intake package

---
+++ evidence_first/intake/__init__.py
@@ -0,0 +1,15 @@
+from .fingerprint import fingerprint_bytes, fingerprint_text
+from .models import IntakeIssue, IntakeRecord, IntakeResult, SourceDescriptor
+from .parser import ingest_csv, ingest_json, ingest_text
+
+__all__ = [
+    "fingerprint_bytes",
+    "fingerprint_text",
+    "IntakeIssue",
+    "IntakeRecord",
+    "IntakeResult",
+    "SourceDescriptor",
+    "ingest_csv",
+    "ingest_json",
+    "ingest_text",
+]
