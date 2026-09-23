f259bebe63625fd6efac7811ce4357cc137158b7
rkalema <rkalema26@gmail.com>
2026-09-23 16:00:53 -0700

    Add evidence_first/adapters/__init__.py

---
+++ evidence_first/adapters/__init__.py
@@ -0,0 +1,5 @@
+from .base import AgentAdapter
+from .callable_adapter import CallableAdapter
+from .prompt_builder import build_agent_prompt
+
+__all__=["AgentAdapter","CallableAdapter","build_agent_prompt"]
