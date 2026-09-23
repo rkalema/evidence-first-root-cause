5c7eaaa280171bbccdb731fe0e2e9310105d48c1
rkalema <rkalema26@gmail.com>
2026-09-23 16:20:41 -0700

    Expose structured model adapter

--- evidence_first/adapters/__init__.py
+++ evidence_first/adapters/__init__.py
@@ -1,5 +1,6 @@
 from .base import AgentAdapter
 from .callable_adapter import CallableAdapter
 from .prompt_builder import build_agent_prompt
+from .json_model import JSONModelAdapter
 
-__all__=["AgentAdapter","CallableAdapter","build_agent_prompt"]
+__all__=["AgentAdapter","CallableAdapter","build_agent_prompt","JSONModelAdapter"]
