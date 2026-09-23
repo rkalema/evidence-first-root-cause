421087adee67f7c5de7ec1446a2b6eea068ec990
rkalema <rkalema26@gmail.com>
2026-09-23 16:10:41 -0700

    Expose Excel intake

--- evidence_first/intake/__init__.py
+++ evidence_first/intake/__init__.py
@@ -1,6 +1,7 @@
 from .fingerprint import fingerprint_bytes, fingerprint_text
 from .models import IntakeIssue, IntakeRecord, IntakeResult, SourceDescriptor
 from .parser import ingest_csv, ingest_json, ingest_text
+from .excel import ingest_xlsx_bytes
 
 __all__ = [
     "fingerprint_bytes",
@@ -12,4 +13,5 @@ __all__ = [
     "ingest_csv",
     "ingest_json",
     "ingest_text",
+    "ingest_xlsx_bytes",
 ]
