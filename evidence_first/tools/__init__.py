14a54840d87ee4dbb8d32661863099ad1e757a08
rkalema <rkalema26@gmail.com>
2026-09-23 16:19:51 -0700

    Update evidence_first/tools/__init__.py exports

--- evidence_first/tools/__init__.py
+++ evidence_first/tools/__init__.py
@@ -2,4 +2,5 @@ from .dataframe import DataFrameTool, AnalysisResult
 from .registry import ToolRegistry, ToolSpec
 from .sql import ReadOnlySQLTool, SQLResult
 from .statistics import Difference, difference, weighted_mean, slope
-__all__=["DataFrameTool","AnalysisResult","ToolRegistry","ToolSpec","ReadOnlySQLTool","SQLResult","Difference","difference","weighted_mean","slope"]
+from .factory import dataframe_registry, sql_registry
+__all__=['DataFrameTool','AnalysisResult','ToolRegistry','ToolSpec','ReadOnlySQLTool','SQLResult','Difference','difference','weighted_mean','slope','dataframe_registry','sql_registry']
