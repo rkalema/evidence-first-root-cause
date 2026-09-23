b60c14c8c733f60422d4e022be21817f78c8abac
rkalema <rkalema26@gmail.com>
2026-09-23 16:16:18 -0700

    Update exports in evidence_first/governance/__init__.py

--- evidence_first/governance/__init__.py
+++ evidence_first/governance/__init__.py
@@ -1,2 +1,3 @@
 from .approval import ActionRequest, ActionRisk, requires_human_approval
-__all__=['ActionRequest','ActionRisk','requires_human_approval']
+from .gates import ConclusionCandidate, GateResult, evaluate_conclusion
+__all__=['ActionRequest','ActionRisk','requires_human_approval','ConclusionCandidate','GateResult','evaluate_conclusion']
