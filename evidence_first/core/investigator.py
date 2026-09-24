c2952ecf53b28eea17273acd32d56ba2b44b55c1
rkalema <rkalema26@gmail.com>
2026-09-23 23:06:53 -0700

    Prevent unsupported causal promotion in summarizer

--- evidence_first/core/investigator.py
+++ evidence_first/core/investigator.py
@@ -25,22 +25,35 @@ def summarize(state: InvestigationState) -> InvestigationSummary:
         HypothesisStatus.NOT_SUPPORTED: [],
         HypothesisStatus.UNTESTED: [],
     }
-    findings = []
+    findings: list[ContradictionFinding] = []
 
     for hypothesis in state.hypotheses.all:
-        hypothesis.evaluate()
-        groups[hypothesis.status].append(hypothesis.hypothesis_id)
+        status = state.hypotheses.evaluate(hypothesis.hypothesis_id)
+        groups[status].append(hypothesis.hypothesis_id)
         findings.append(review_hypothesis(hypothesis))
 
-    if state.status is InvestigationStatus.OPEN:
-        if len(groups[HypothesisStatus.SUPPORTED]) == 1:
-            status = InvestigationStatus.ROOT_CAUSE_SUPPORTED
-        elif len(groups[HypothesisStatus.SUPPORTED]) > 1:
-            status = InvestigationStatus.MULTIPLE_CONTRIBUTORS_SUPPORTED
-        else:
+    status = state.status
+    if status is InvestigationStatus.OPEN:
+        if not state.signal_validated:
             status = InvestigationStatus.INSUFFICIENT_EVIDENCE
-    else:
-        status = state.status
+        elif state.hypotheses.invalid_evidence_refs:
+            status = InvestigationStatus.INSUFFICIENT_EVIDENCE
+        elif len(state.hypotheses.all) < state.minimum_competing_hypotheses:
+            status = InvestigationStatus.INSUFFICIENT_EVIDENCE
+        else:
+            supported = [
+                state.hypotheses.get(hid)
+                for hid in groups[HypothesisStatus.SUPPORTED]
+            ]
+            if len(supported) == 1:
+                status = InvestigationStatus.ROOT_CAUSE_SUPPORTED
+            elif len(supported) > 1:
+                if all(h.relationship == "additive" for h in supported):
+                    status = InvestigationStatus.MULTIPLE_CONTRIBUTORS_SUPPORTED
+                else:
+                    status = InvestigationStatus.INSUFFICIENT_EVIDENCE
+            else:
+                status = InvestigationStatus.INSUFFICIENT_EVIDENCE
 
     return InvestigationSummary(
         status=status,
